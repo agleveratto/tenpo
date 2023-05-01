76fff53686653ea105eae8372a82108aed271c35
agleveratto <agleveratto@gmail.com>
2023-05-01 12:37:07 -0300

    feat: implements percentage service to sum service

--- src/main/java/com/agl/tenpo/domain/services/SumServiceImpl.java
+++ src/main/java/com/agl/tenpo/domain/services/SumServiceImpl.java
@@ -1,13 +1,34 @@
 package com.agl.tenpo.domain.services;
 
 import com.agl.tenpo.application.services.SumService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 public class SumServiceImpl implements SumService {
 
+    private final RestTemplate restTemplate;
+
+    @Value("${percentage.service.url}")
+    private String PERCENTAGE_SERVICE_URL;
+
+    public SumServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @Override
     public double sum(int num1, int num2) {
-        return num1+num2;
+        int percentage = getExternalPercentage();
+        log.info("percentage getted {}", percentage);
+        int sum = num1 + num2;
+        return sum + ((double) (percentage * sum) / 100);
+    }
+
+    private Integer getExternalPercentage() {
+        log.info("getting percentage from external service");
+        return restTemplate.getForObject(PERCENTAGE_SERVICE_URL, Integer.class);
     }
 }
--- src/test/java/com/agl/tenpo/domain/services/SumServiceTest.java
+++ src/test/java/com/agl/tenpo/domain/services/SumServiceTest.java
@@ -1,22 +1,43 @@
 package com.agl.tenpo.domain.services;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class SumServiceTest {
 
+    private static final String PERCENTAGE_SERVICE_URL = "http://localhost:8080/api/v1/percentage/";
     @InjectMocks
     SumServiceImpl sumService;
 
+    @Mock
+    private RestTemplate restTemplate;
+
+    @BeforeEach
+    void setup(){
+        setReflections();
+    }
+
+    private void setReflections() {
+        ReflectionTestUtils.setField(sumService, "PERCENTAGE_SERVICE_URL", PERCENTAGE_SERVICE_URL);
+    }
+
     @Test
     void sumNumbers(){
-        double result = sumService.sum(1, 2);
+        when(restTemplate.getForObject(PERCENTAGE_SERVICE_URL, Integer.class)).thenReturn(81);
 
-        assertThat(result).isEqualTo(3.0);
+        assertThat(sumService.sum(20,23)).isEqualTo(77.83);
+
+        verify(restTemplate).getForObject(PERCENTAGE_SERVICE_URL, Integer.class);
     }
 }
