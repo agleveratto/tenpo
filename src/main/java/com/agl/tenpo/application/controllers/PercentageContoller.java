bf0c0672673a7080190513e2f7d4c3e7165127e1
agleveratto <agleveratto@gmail.com>
2023-05-01 15:01:31 -0300

    feat: implement evict cache every 30 mins

--- src/main/java/com/agl/tenpo/application/controllers/PercentageContoller.java
+++ src/main/java/com/agl/tenpo/application/controllers/PercentageContoller.java
@@ -1,7 +1,6 @@
 package com.agl.tenpo.application.controllers;
 
 import com.agl.tenpo.application.services.PercentageService;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +18,6 @@ public class PercentageContoller {
     }
 
     @GetMapping("/")
-    @Cacheable(value = "percentage")
     public ResponseEntity<Integer> retrievePercentage(){
         return new ResponseEntity<>(percentageService.retrievePercentage(), HttpStatus.OK);
     }
--- src/main/java/com/agl/tenpo/domain/services/PercentageServiceImpl.java
+++ src/main/java/com/agl/tenpo/domain/services/PercentageServiceImpl.java
@@ -1,12 +1,26 @@
 package com.agl.tenpo.domain.services;
 
 import com.agl.tenpo.application.services.PercentageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
+@CacheConfig(cacheNames = "percentage")
 public class PercentageServiceImpl implements PercentageService {
     @Override
+    @Cacheable
     public int retrievePercentage() {
         return (int) (Math.random()*100+1);
+    }
+
+    @Scheduled(fixedRate = 1800000)
+    @CacheEvict(allEntries = true)
+    public void clearCache(){
+        log.info("clearing cache");
     }
 }
